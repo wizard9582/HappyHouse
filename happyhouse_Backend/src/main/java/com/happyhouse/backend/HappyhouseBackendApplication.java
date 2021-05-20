ca757c2d8708f5fb79de09d403df61215b89e798
qkfskan82 <qkfskan82@naver.com>
2021-05-20 13:04:34 +0900

    Spring Backend create

---
+++ happyhouse_Backend/src/main/java/com/happyhouse/backend/HappyhouseBackendApplication.java
@@ -0,0 +1,13 @@
+package com.happyhouse.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappyhouseBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HappyhouseBackendApplication.class, args);
+	}
+
+}
