d9d9486f724415df2f2f4360164e5ceb24d2587c
jaewon <jwsims@naver.com>
2021-05-21 14:33:03 +0900

    Revert "1"
    This reverts commit 4d3c1e6e84cc156cdaadc38d2fd7ae227ddc5c13.

--- happyhouse_Backend/src/main/java/com/happyhouse/backend/HappyhouseBackendApplication.java
+++ happyhouse_Backend/src/main/java/com/happyhouse/backend/HappyhouseBackendApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HappyHouseFrameworkApplication {
+public class HappyhouseBackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HappyHouseFrameworkApplication.class, args);
+		SpringApplication.run(HappyhouseBackendApplication.class, args);
 	}
 
 }
