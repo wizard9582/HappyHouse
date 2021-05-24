5b7bebd67d7c322a0daeb5e1512a1a17e0c59cc3
qkfskan82 <qkfskan82@naver.com>
2021-05-24 14:41:49 +0900

    kakao map event template 완성

--- happyhouse/src/components/map/map.vue
+++ happyhouse/src/components/map/map.vue
@@ -22,17 +22,46 @@ export default {
     },
     methods: {
     initMap () {
+
+        var iwContent = '<div style="padding:5px;">Hello World!</div>';
+        var infowindow = new kakao.maps.InfoWindow({
+            content : iwContent
+        });
+
+
         const container = document.querySelector('#kakaomap')
         const options = {
-        center: new kakao.maps.LatLng(35.19656853772262, 129.0807270648317),
+        center: new kakao.maps.LatLng(37.5666805, 126.9784147),
         level: 3
         }
         const map = new kakao.maps.Map(container, options)
-        const markerPosition = new kakao.maps.LatLng(35.19656853772262, 129.0807270648317);
-
+        const markerPosition = new kakao.maps.LatLng(37.5666805, 126.9784147);
+        
+        const mapTypeControl = new kakao.maps.MapTypeControl();
+        map.addControl(mapTypeControl, kakao.maps.ControlPosition.TOPLEFT);
+        const zoomControl = new kakao.maps.ZoomControl();
+        map.addControl(zoomControl, kakao.maps.ControlPosition.LEFT);
+        
         const marker = new kakao.maps.Marker({
         position: markerPosition
         });
+
+        kakao.maps.event.addListener(marker, 'click', function() {
+		//alert('마커를 클릭했습니다!');
+		});
+
+
+		kakao.maps.event.addListener(marker, 'mouseover', function() {
+		//console.log('마커에 mouseover 이벤트가 발생했습니다!');
+        infowindow.open(map, marker);
+		});
+
+
+		kakao.maps.event.addListener(marker, 'mouseout', function() {
+		//console.log('마커에 mouseout 이벤트가 발생했습니다!');
+        infowindow.close();
+		});
+
         marker.setMap(map)
         }
     }
